7a9288bbbaaa68d4c93c2a751151fb1ccaf0000e
DawnCavalier <tyler.toper@gmail.com>
2026-07-08 22:21:07 -0400

    start of game engine

---
+++ v2/gameengine.py
@@ -0,0 +1,18 @@
+from queue import Queue
+
+class GameEngine:
+    def __init__(self):
+        self.workQueue = Queue()
+        return
+
+    
+    
+def func():
+    print(f"func")
+
+def main():
+    engine = GameEngine()
+    
+
+if __name__ == "__main__":
+    main()
