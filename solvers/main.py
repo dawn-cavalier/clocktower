efed131867213db492c081309efcea2354f19950
Ashley Toper <ashley.r.toper@gmail.com>
2026-09-22 14:15:30 -0400

    solver scoring print

--- solvers/main.py
+++ solvers/main.py
@@ -9,29 +9,38 @@ def main() -> None:
 
     # TODO: Handle different evil counts
     for i in range(PLAYER_COUNT):
-        for j in range(i + 1 , PLAYER_COUNT):
+        for j in range(i + 1, PLAYER_COUNT):
             for k in range(j + 1, PLAYER_COUNT):
                 evil_teams[(i, j, k)] = 0.0
 
-    # playerIsGood(evil_teams, 0)
+    setChefInfo(evil_teams, 0)
+
+    playerIsGood(evil_teams, 0)
     # playerIsEvil(evil_teams, 11)
 
-    setChefInfo(evil_teams, 2)
+    # for key, value in evil_teams.items():
+    #     if value < 0.01:
+    #         continue
+    #     print(f"{key}: {value}")
 
-    for key, value in evil_teams.items():
-        if value < 0.01:
-            continue
-        print(f"{key}: {value}")
+    for player in range(PLAYER_COUNT):
+        print(f"{player}: {getPlayerScore(evil_teams, player)}")
 
-
-def playerIsGood(evil_teams: dict[tuple[int, int, int], float], target_player: int):
+def getPlayerScore(evil_teams: dict[tuple[int, int, int], float], target_player: int) -> float:
+    score = 0
     for key in [key for key in evil_teams if target_player in key]:
-        evil_teams[key] += -math.inf
+        score += evil_teams[key]
+
+    return score
+
+def playerIsGood(evil_teams: dict[tuple[int, int, int], float], target_player: int) -> None:
+    for key in [key for key in evil_teams if target_player in key]:
+        evil_teams[key] += -evil_teams[key]
 
 
-def playerIsEvil(evil_teams: dict[tuple[int, int, int], float], target_player: int):
+def playerIsEvil(evil_teams: dict[tuple[int, int, int], float], target_player: int) -> None:
     for key in [key for key in evil_teams if target_player not in key]:
-        evil_teams[key] += -math.inf
+        evil_teams[key] += -evil_teams[key]
 
 
 def setChefInfo(
@@ -48,7 +57,6 @@ def setChefInfo(
                     continue
 
                 evil_teams[key] += 1
-            return
         case 1:
             for key in evil_teams:
                 # Right/ no overflow
@@ -69,7 +77,6 @@ def setChefInfo(
                     or key[2] + 1 - PLAYER_COUNT == key[0]
                 ):
                     evil_teams[key] += 1
-            return
         case 2:
             for key in evil_teams:
                 # Right/ no overflow
@@ -83,7 +90,6 @@ def setChefInfo(
                 # Left / overflow
                 if key[0] == key[1] + 2 - PLAYER_COUNT == key[2] + 1 - PLAYER_COUNT:
                     evil_teams[key] += 1
-            return
         case _:
             return
 
